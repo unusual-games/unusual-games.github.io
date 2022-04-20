01ae1dec20eb934c94b803a7fb393c793c69e63d
unusual-dev <unusual.dev.business@gmail.com>
2022-04-20 21:51:44 +1000

    update

---
+++ links.js
@@ -0,0 +1,44 @@
+var links = [
+    {
+        "name": "Lost in the Static",
+        "info": "A weird game made entirely out of static, yet strangely has a cool vibe to it.",
+        "image": "images/lostinthestatic.png",
+        "link": "https://silverspaceship.com/static/",
+        "gameplay": ""
+    },
+    {
+        "name": "3-manifold",
+        "info": "A cool kinda liminal space game with so many portals it's crazy.",
+        "image": "images/3-manifold.png",
+        "link": "https://www.youtube.com/watch?v=CBPTcUxIISQ",
+        "gameplay": ""
+    },
+    {
+        "name": "Space Scrap Shuffle",
+        "info": "A game where you destroy space ships and take parts to customize your space ship.",
+        "image": "images/spacescrapshuffle.png",
+        "link": "https://finite-reflection.itch.io/space-scrap-shuffle",
+        "gameplay": ""
+    },
+    {
+        "name": "Asciicker",
+        "info": "A cool online game made entirely out of ascii characters (still in development).",
+        "image": "images/asciicker.png",
+        "link": "https://asciicker.com/y6/",
+        "gameplay": ""
+    },
+    {
+        "name": "Sinuous Game",
+        "info": "A highly addictive experience, where you dodge enemies and grab powerups!",
+        "image": "images/sinuousgame.png",
+        "link": "http://www.sinuousgame.com/",
+        "gameplay": ""
+    },
+    {
+        "name": "Gravity Points",
+        "info": "Not really a game but a cool simulation of gravity between many points.",
+        "image": "images/gravitypoints.png",
+        "link": "https://codepen.io/akm2/full/AGgarW",
+        "gameplay": ""
+    }
+]
