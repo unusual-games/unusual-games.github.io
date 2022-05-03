af108b1a9cce7fd33618e2fde0ec7b909e4bc638
unusual-dev <unusual.dev.business@gmail.com>
2022-05-03 14:25:07 +1000

    update

--- links.js
+++ links.js
@@ -4,41 +4,41 @@ var links = [
         "info": "A weird game made entirely out of static, yet strangely has a cool vibe to it.",
         "image": "images/lostinthestatic.png",
         "link": "https://silverspaceship.com/static/",
-        "gameplay": ""
+        "gameplay": "https://www.youtube.com/watch?v=MJ-tuvxCOiQ"
     },
     {
         "name": "3-manifold",
         "info": "A cool kinda liminal space game with so many portals it's crazy.",
         "image": "images/3-manifold.png",
         "link": "https://www.youtube.com/watch?v=CBPTcUxIISQ",
-        "gameplay": ""
+        "gameplay": "https://www.youtube.com/watch?v=NwkQ7eVzD0k"
     },
     {
         "name": "Space Scrap Shuffle",
         "info": "A game where you destroy space ships and take parts to customize your space ship.",
         "image": "images/spacescrapshuffle.png",
         "link": "https://finite-reflection.itch.io/space-scrap-shuffle",
-        "gameplay": ""
+        "gameplay": "https://www.youtube.com/watch?v=av0vzBMo6H0"
     },
     {
         "name": "Asciicker",
         "info": "A cool online game made entirely out of ascii characters (still in development).",
         "image": "images/asciicker.png",
         "link": "https://asciicker.com/y6/",
-        "gameplay": ""
+        "gameplay": "https://www.youtube.com/watch?v=bY3Wzv0WaFo"
     },
     {
         "name": "Sinuous Game",
         "info": "A highly addictive experience, where you dodge enemies and grab powerups!",
         "image": "images/sinuousgame.png",
         "link": "http://www.sinuousgame.com/",
-        "gameplay": ""
+        "gameplay": "https://www.youtube.com/watch?v=cZiidGZ_dy8"
     },
     {
         "name": "Gravity Points",
         "info": "Not really a game but a cool simulation of gravity between many points.",
         "image": "images/gravitypoints.png",
         "link": "https://codepen.io/akm2/full/AGgarW",
-        "gameplay": ""
+        "gameplay": "https://www.youtube.com/watch?v=v_Mh0uOB-o0"
     }
 ]
